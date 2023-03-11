29c17c4f9e2789810557d9952b24a46da0b4640a
CwjNavi <ivan_chan@mymail.sutd.edu.sg>
2023-03-11 23:15:59 +0800

    First commit

--- app/src/main/java/com/example/a1d_project/AllJourneysActivity.java
+++ app/src/main/java/com/example/a1d_project/AllJourneysActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AllJourneys extends AppCompatActivity {
+public class AllJourneysActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
